90506041efa3f57be92eb831e8da00824d5e779e
Gareth Bradley <garfbradaz@outlook.com>
2021-06-06 08:54:41 +0100

    Handler now has the logic needed to calculate the TotalCost of the orderlines.
    In reality we would have an IRepository<TAggregateRoot> which would persist this order line for prospertity.

--- src/FruitCart.Checkout/Command.PlaceOrder/PlaceOrderHandler.cs
+++ src/FruitCart.Checkout/Command.PlaceOrder/PlaceOrderHandler.cs
@@ -1,14 +1,27 @@
 using System.Threading;
 using System.Threading.Tasks;
+using FruitCart.Checkout.Command.PlaceOrder.Factories;
 using MediatR;
 
 namespace FruitCart.Checkout.Command.PlaceOrder
 {
     public class PlaceOrderHandler : IRequestHandler<PlaceOrderCommand, PlaceOrderResponse>
     {
+        private readonly IOrderEntityFactory orderEntityFactory;
+        
+        public PlaceOrderHandler(IOrderEntityFactory orderEntityFactory)
+        {
+            this.orderEntityFactory = orderEntityFactory;
+        }
+
         public Task<PlaceOrderResponse> Handle(PlaceOrderCommand request, CancellationToken cancellationToken)
         {
-            return Task.FromResult(PlaceOrderResponse.CreateEmpty());
+            var order = this.orderEntityFactory.Create(request.Fruits);
+
+            order.CalculateTotalCost();
+
+            return Task.FromResult(PlaceOrderResponse.Create(order.TotalCost.Value));
         }
+
     }
 }
