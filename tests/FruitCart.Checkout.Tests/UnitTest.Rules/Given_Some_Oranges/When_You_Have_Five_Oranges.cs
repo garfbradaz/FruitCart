cad6fcb81ef0d201ce2fc401843b974810decf47
Gareth Bradley <garfbradaz@outlook.com>
2021-06-06 22:04:52 +0100

    3 for the price of 2 for Oranges Rules and tests.

---
+++ tests/FruitCart.Checkout.Tests/UnitTest.Rules/Given_Some_Oranges/When_You_Have_Five_Oranges.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using FluentAssertions;
+using FruitCart.Checkout.Tests.Shared.Extensions;
+using Xunit;
+
+namespace FruitCart.Checkout.Tests.UnitTest.Rules.Given_Some_Oranges
+{
+    public class When_You_Have_Five_Oranges : IClassFixture<RuleFixture>
+    {
+        private const int totalOranges = 5;
+
+        private readonly RuleFixture fixture;
+
+        public When_You_Have_Five_Oranges(RuleFixture fixture)
+        {
+            this.fixture = fixture;
+            Act();
+        }
+
+        [Fact]
+        public void It_Order_Should_Not_Be_Null()
+        {
+            this.fixture.Order.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void It_Should_Have_The_Correct_Orderlines()
+        {
+            this.fixture.Order.OrderLines.Count().Should().Be(totalOranges);
+        }
+
+        [Fact]
+        public void It_Total_Cost_Should_Be_The_Correct_Price()
+        {
+            this.fixture.Result.Should().Be(1.00m);
+        }
+
+        public void Act()
+        {
+            this.fixture.Order = this.fixture.Factory.Create(FruitData.WithXAmountOf(totalOranges, "Orange"));
+            this.fixture.Result = this.fixture.Sut.ReCalculateTotalCost(this.fixture.Order);
+        }
+    }
+}
